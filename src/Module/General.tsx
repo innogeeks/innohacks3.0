affac250d0057a0eb63ed428ad3015d0fd5efdbb
Geek-shreya <gargshreya003@gmail.com>
2024-02-21 23:22:17 +0530

    content

--- src/Module/General.tsx
+++ src/Module/General.tsx
@@ -68,7 +68,7 @@ const TOP_SECTION = {
   TITLE: 'Join InnoHacks3.0',
   Typed_effect: ['24 hours of Innovation', 'Win awesome prizes'],
   SHORT_DESCRIPTION:
-    'Join us on 27th April 2024 with over Hundreds of students from across the nation for 24 hours of creation, innovation, & fun.',
+    'Unleash your creativity for 24 hours! Join us on April 27th for a nationwide hackathon alongside hundreds of other student innovators. Build, learn, and connect - are you in?',
   IMG_SRC: hackathonLogo,
   DISCORD_LINK: '',
   JUDGES_FORM_LINK:
@@ -90,7 +90,7 @@ const SOCIALS = {
 const MIDDLE_SECTION = {
   TITLE: 'What is InnoHacks?',
   LONG_DESCRIPTION:
-    'The largest code feast of Delhi NCR is back! Join a 24 Hour Hackathon to hustle and shine through 5 amazing tracks, Embark on an Odyssey to learn new technologies in captivating environment seek guidance from experienced mentors and engrave your name on boards of our esteemed Judges! Not just that, win huge prizes and hampers by brainstorming and creating the best hack! Be ready to sail through an amazing night of fun, code and much more!',
+    'The largest code fest of Delhi NCR is back! Join a 24 Hour Hackathon to hustle and shine through 5 amazing tracks, Embark on an Odyssey to learn new technologies in captivating environment seek guidance from experienced mentors and engrave your name on boards of our esteemed Judges! Not just that, win huge prizes and hampers by brainstorming and creating the best hack! Be ready to sail through an amazing night of fun, code and much more!',
   LOGO: hackathonLogo
 };
 
