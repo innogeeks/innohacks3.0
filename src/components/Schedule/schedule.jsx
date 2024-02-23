38e6a2ce9dae5e1ccbc7cdf21b8f734ae5d76ec3
Praveen Prakash Pal <palprakashpraveen@gmail.com>
2024-02-23 17:13:17 +0530

    made all the changes

--- src/components/Schedule/schedule.jsx
+++ src/components/Schedule/schedule.jsx
@@ -30,7 +30,7 @@ return(
             <div className="content">
               <h3 className="title">Start Hacking !</h3>
               <p className="description">
-                The Fun begins ! Start your systems and Get ready for an adventure filled with code and adventure.
+                The Fun begins ! Start your systems and Get ready for an adventure filled with collaboration and code.
               </p>
             </div>
           </a>
